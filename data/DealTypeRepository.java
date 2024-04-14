3f5e03fed4a594fc228c990cefb1e69af8cd36c3
Maria <belldom666@yandex.ru>
2024-04-14 14:32:29 +0300

    commit

--- data/DealTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package fin.data;
-
-import org.springframework.data.repository.CrudRepository;
-import fin.DealType;
-
-public interface DealTypeRepository extends CrudRepository<DealType, Long> {
-}
--- data/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package fin.data;
-import org.springframework.data.repository.CrudRepository;
-import fin.TacoOrder;
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
-}
-
